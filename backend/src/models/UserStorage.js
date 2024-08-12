315fce0e29c26a5e2d467f65fd45b82323f668cb
pigggulggul <impigggulggul@naver.com>
2024-08-12 16:57:52 +0900

    feat: tailwind 설정

---
+++ backend/src/models/UserStorage.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const db = require("../config/db");
+
+//UserStorage에서는 DB를 CRUD 기능만 함.
+class UserStorage {
+  static getUserInfo(id) {
+    // Promise 안의 구문이 성공하면 resolve를 실행하고 실패시 reject를 실행한다.
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM users where id=?;";
+      db.query(query, [id], (err, data) => {
+        if (err) reject(`${err}`);
+        else resolve(data[0]);
+      });
+    });
+  }
+  static async save(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO users(id, name, psword) VALUES (?, ?, ?);";
+      db.query(query, [userInfo.id, userInfo.name, userInfo.psword], (err) => {
+        if (err) reject(`${err}`);
+        else resolve({ success: true });
+      });
+    });
+  }
+}
+module.exports = UserStorage;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <div className="flex flex-col justify-center items-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +28,21 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex items-center">
+          <p className="me-2">ID</p>
+          <input type="text" className="px-2 bg-slate-200 my-2" />
+        </div>
+        <div className="flex items-center">
+          <p className="me-2">PW</p>
+          <input type="text" className="px-2 bg-slate-200 my-2" />
+        </div>
+        <div className=" bg-slate-300 px-4 py-2 rounded-md cursor-pointer">
+          등록
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
